35f8a645e392a54b0076ba36dea1794163609402
MrDave1999 <daveseva2010@hotmail.es>
2022-11-18 19:15:46 -0500

    Added the structure of the test project

---
+++ tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,10 @@
+namespace DotEnv.Core.Extensions.Microsoft.DependencyInjection.Tests;
+
+[TestClass]
+public class ServiceCollectionExtensionsTests
+{
+    [TestMethod]
+    public void Method1()
+    {
+    }
+}
